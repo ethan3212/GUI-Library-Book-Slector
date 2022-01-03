1fbd1a1fcf8d4e645e5043be6d0b1412180ca058
ethan3212 <74456094+ethan3212@users.noreply.github.com>
2022-01-03 09:02:35 -0600

    Add files via upload

---
+++ Manis_FinalProject_Q2_MyLibrary/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace Manis_FinalProject_Q2_MyLibrary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bookListBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.outPutLabel = new System.Windows.Forms.Label();
+            this.selectionButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bookListBox
+            // 
+            this.bookListBox.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.bookListBox.Font = new System.Drawing.Font("Lucida Calligraphy", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bookListBox.FormattingEnabled = true;
+            this.bookListBox.ItemHeight = 27;
+            this.bookListBox.Items.AddRange(new object[] {
+            "The Lord of the Rings: The Fellowship of the Ring",
+            "the Lord of the Rings: The Two Towers",
+            "The Lord of the Rings The Return of the King",
+            "The Hobbit",
+            "Maze Runner",
+            "The Hunger Games",
+            "Fight Club"});
+            this.bookListBox.Location = new System.Drawing.Point(236, 106);
+            this.bookListBox.Name = "bookListBox";
+            this.bookListBox.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.bookListBox.Size = new System.Drawing.Size(632, 220);
+            this.bookListBox.TabIndex = 0;
+            this.bookListBox.SelectedIndexChanged += new System.EventHandler(this.bookListBox_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Calligraphy", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1058, 36);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Make as many selections as you like by holding down the Ctrl key";
+            // 
+            // outPutLabel
+            // 
+            this.outPutLabel.AutoSize = true;
+            this.outPutLabel.Font = new System.Drawing.Font("Lucida Calligraphy", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outPutLabel.Location = new System.Drawing.Point(232, 475);
+            this.outPutLabel.Name = "outPutLabel";
+            this.outPutLabel.Size = new System.Drawing.Size(0, 27);
+            this.outPutLabel.TabIndex = 2;
+            // 
+            // selectionButton
+            // 
+            this.selectionButton.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.selectionButton.Font = new System.Drawing.Font("Lucida Calligraphy", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.selectionButton.Location = new System.Drawing.Point(452, 346);
+            this.selectionButton.Name = "selectionButton";
+            this.selectionButton.Size = new System.Drawing.Size(186, 93);
+            this.selectionButton.TabIndex = 3;
+            this.selectionButton.Text = "Make Selection";
+            this.selectionButton.UseVisualStyleBackColor = false;
+            this.selectionButton.Click += new System.EventHandler(this.selectionButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1091, 590);
+            this.Controls.Add(this.selectionButton);
+            this.Controls.Add(this.outPutLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bookListBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox bookListBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label outPutLabel;
+        private System.Windows.Forms.Button selectionButton;
+    }
+}
+
---
+++ Manis_FinalProject_Q2_MyLibrary/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Manis_FinalProject_Q2_MyLibrary
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bookListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void selectionButton_Click(object sender, EventArgs e)
+        {
+            const double PRICE = 3.99;
+            int COUNT = bookListBox.SelectedItems.Count;
+            double Price = COUNT * PRICE;
+            outPutLabel.Text = COUNT + " Selections, price is: " + Price.ToString("c");
+        }
+    }
+}
